32263197bbeb15084fd79267d8542d4ff2bc4a25
Marcos Luperce Rocha <vitornegraorocha@gmail.com>
2025-12-20 14:53:38 -0300

    Adiciona ícone do navegador

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
                     className="flex items-center gap-2 cursor-pointer"
                     onClick={() => scrollToSection("home")}
                 >
-                    <div className="w-12 h-12 rounded-lg flex items-center justify-center">
+                    <div className="w-14 h-14 rounded-lg flex items-center justify-center">
                         <img src="/footer-logo.png" alt="Logo" className="w-full h-full object-contain" />
                     </div>
                     <span className="font-display font-bold text-lg">Gestor de Tráfego</span>
