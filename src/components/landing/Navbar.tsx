69e6204981d47e5a793a0c576f0d0fbeb1f56d2b
Marcos Luperce Rocha <vitornegraorocha@gmail.com>
2025-12-20 15:04:39 -0300

    Muda nome

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
                     <div className="w-14 h-14 rounded-lg flex items-center justify-center">
                         <img src="/footer-logo.png" alt="Logo" className="w-full h-full object-contain" />
                     </div>
-                    <span className="font-display font-bold text-lg">Gestor de Tráfego</span>
+                    <span className="font-display font-bold text-lg">Vitor Negrão</span>
                 </div>
 
                 {/* Desktop Menu */}
